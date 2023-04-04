123452f744a85d7718f851ba08b8c41a4f7e0cd2
adomingueztemola <103951211+adomingueztemola@users.noreply.github.com>
2023-04-04 08:25:51 -0600

    Total de Empaque del dia tablero

--- templates/Rendimiento/Estadistica/cargaTableroCajas.php
+++ templates/Rendimiento/Estadistica/cargaTableroCajas.php
@@ -12,51 +12,64 @@ $obj_empaque = new Empaque($debug, $idUser);
 $fecha = (!empty($_POST['fecha']) and $_POST['fecha'] != '') ? $_POST['fecha'] : date('Y-m-d');
 $Data = $obj_empaque->getCajasCompletXPrograma($fecha);
 $Data = Excepciones::validaConsulta($Data);
+$totalCajas=0;
 ?>
-<div class="table-responsive">
-    <table class="table table-sm  table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>Programa</th>
-                <th>Uso</th>
-
-                <th>Cantidad</th>
-            </tr>
-
-        </thead>
-
-
-        <tbody>
-            <?php
-            if (count($Data) <= 0) {
-                echo "<tr>
-                    <td colspan='3' class='text-center'>Sin Registro de Caja Localizado</td>
-                </tr>";
-            } else {
-                foreach ($Data as  $value) {
-                    $fTotalCajas = formatoMil($value['totalCajas'], 0);
-                    $cajasGrafico = str_repeat('<input size="1" class="bg-success" disabled>', $value['totalCajas']);
-                    $lblInterno = $value['interna'] == '1' ? 'Interna' : '-';
-            ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="table-responsive">
+            <table class="table table-sm  table-striped table-bordered">
+                <thead>
                     <tr>
-                        <!-- primer resultado-->
-                        <th><?= $value['nPrograma'] ?>
-                            <?= $cajasGrafico ?>
-                        </th>
+                        <th>Programa</th>
+                        <th>Uso</th>
 
-                        <td>
-                            <?= $lblInterno ?>
-                        </td>
-                        <td>
-                            <?= $fTotalCajas ?>
-                        </td>
-
+                        <th>Cantidad</th>
                     </tr>
 
-            <?php
-                }
-            }
-            ?>
-        </tbody>
-    </table>
+                </thead>
+
+
+                <tbody>
+                    <?php
+                    if (count($Data) <= 0) {
+                        echo "<tr>
+                    <td colspan='3' class='text-center'>Sin Registro de Caja Localizado</td>
+                </tr>";
+                    } else {
+                        foreach ($Data as  $value) {
+                            $fTotalCajas = formatoMil($value['totalCajas'], 0);
+                            $cajasGrafico = str_repeat('<input size="1" class="bg-success" disabled>', $value['totalCajas']);
+                            $lblInterno = $value['interna'] == '1' ? 'Interna' : '-';
+                            $totalCajas=$totalCajas+$value['totalCajas'];
+                    ?>
+                            <tr>
+                                <!-- primer resultado-->
+                                <th><?= $value['nPrograma'] ?>
+                                    <?= $cajasGrafico ?>
+                                </th>
+
+                                <td>
+                                    <?= $lblInterno ?>
+                                </td>
+                                <td>
+                                    <?= $fTotalCajas ?>
+                                </td>
+
+                            </tr>
+
+                    <?php
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card text-white bg-success" >
+            <div class="card-header text-center"><h5>Total de Empaque del día: <?=formatoMil($totalCajas, 0)?></h5></div>
+        </div>
+    </div>
 </div>
