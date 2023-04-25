9e35f1cb64d6c3f8591f04b03f3561c0a0514008
adomingueztemola <103951211+adomingueztemola@users.noreply.github.com>
2023-04-25 16:29:46 -0600

    Tabla actual de Almacen PT

--- templates/Almacen/tablaAlmacen.php
+++ templates/Almacen/tablaAlmacen.php
@@ -2,38 +2,155 @@
 session_start();
 define('INCLUDE_CHECK', 1);
 require_once "../../include/connect_mvc.php";
+include('../../Models/Mdl_ConexionBD.php');
+include('../../Models/Mdl_Rendimiento.php');
+include('../../assets/scripts/cadenas.php');
 
 $debug = 0;
 $idUser = $_SESSION['CREident'];
+$obj_rendimiento = new Rendimiento($debug, $idUser);
 if ($debug == 1) {
     print_r($_POST);
     //  exit(0);
 } else {
     error_reporting(0);
 }
+$materiaPrima = !empty($_POST['materiaPrima']) ? $_POST['materiaPrima'] : "";
+$procesos = !empty($_POST['procesos']) ? $_POST['procesos'] :  "";
+$programas = !empty($_POST['programas']) ? $_POST['programas'] : '';
+$tipo = !empty($_POST['tipo']) ? $_POST['tipo'] : '';
 
+
+$filtradoMateria = $materiaPrima != '' ? "r.idCatMateriaPrima='$materiaPrima'" : "1=1";
+$filtradoProcesos = $procesos != '' ? "r.idCatProceso='$procesos'" : "1=1";
+$filtradoPrograma = $programas != '' ? "r.idCatPrograma='$programas'" : "1=1";
+$filtradoTipo = $tipo != '' ? "r.tipoProceso='$tipo'" : "1=1";
+
+
+
+
+$DataLotes = $obj_rendimiento->getLotesEnAlmacen($filtradoMateria, $filtradoProcesos, $filtradoPrograma, $filtradoTipo);
 ?>
 <div class="table-responsive">
-    <table id="table-cajas" class="table table-sm">
+    <table id="table-pedidos" class="table table-sm">
         <thead>
             <tr class="">
+                <th>#</th>
+                <th>Tipo</th>
+                <th>Semana Prod.</th>
                 <th>Lote</th>
                 <th>Programa</th>
-                <th>Cajas</th>
+                
+                <th>Proceso de Secado</th>
+                <th>Materia Prima</th>
+                <th>Num. Factura</th>
+                <th>Proveedor</th>
+                <th>Unidad(es)</th>
+                <th>Usuario Registro</th>
+                <th>Fecha Registro</th>
+
             </tr>
         </thead>
-     
+        <tbody>
+            <?php
+            $count = 0;
+            $suma_unidades = 0;
+            foreach ($DataLotes as $key => $value) {
+                $count++;
+                $suma_unidades += $DataLotes[$key]['pzasTotales'];
+                $lbl_tipo= $DataLotes[$key]['tipoProceso']=='1'?'Sets':'M<sup>2</sup>';
+            ?>
+                <tr>
+                    <td><?= $count ?></td>
+                    <td><?= $lbl_tipo ?></td>
+                    <td><?= $DataLotes[$key]['semanaProduccion'] ?></td>
+
+                    <td><b><?= $DataLotes[$key]['loteTemola'] ?></b></td>
+                    <td><?= $DataLotes[$key]['n_programa'] ?></td>
+                    <td><?= $DataLotes[$key]['n_proceso'] ?></td>
+                    <td><?= $DataLotes[$key]['n_materiaprima'] ?></td>
+                    <td><?= $DataLotes[$key]['numFactura'] ?></td>
+                    <td><?= $DataLotes[$key]['n_proveedor'] ?></td>
+
+                    <td><?= formatoMil($DataLotes[$key]['pzasTotales']) ?></td>
+                    <td><?= $DataLotes[$key]['str_usuario'] ?></td>
+                    <td><?= $DataLotes[$key]['f_fechaReg'] ?></td>
+
+
+
+                </tr>
+            <?php
+
+
+            }
+            ?>
+
+        </tbody>
+        <tfoot>
+            <tr class="bg-TWM text-white">
+                <td>Totales:</td>
+                <td>-</td>
+                <td>-</td>
+
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td><?= formatoMil($suma_unidades) ?></td>
+                <td>-</td>
+                <td>-</td>
+
+
+
+
+            </tr>
+        </tfoot>
     </table>
 </div>
 
 
 <script>
-    $("#table-cajas").DataTable({
-        ajax: {
-            "url": "../Controller/empaque.php?op=getcajasempacadas",
-            "type": "POST"
-        },
+    $("#table-pedidos").DataTable({
+            dom: 'Bfrltip',
+
+
+            buttons: [{
+                extend: 'copy',
+                text: 'Copiar Formato',
+                exportOptions: {
+
+                },
+                footer: true
+            }, {
+                extend: 'excel',
+                text: 'Excel',
+                exportOptions: {
+
+                },
+                footer: true
+
+            }, {
+                extend: 'pdf',
+                text: 'Archivo PDF',
+                exportOptions: {
+
+                },
+                orientation: "landscape",
+                footer: true
+
+            }, {
+                extend: 'print',
+                text: 'Imprimir',
+                exportOptions: {
+
+                },
+                footer: true
+
+            }]
         }
 
     );
+    $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-TWM mr-1');
 </script>
