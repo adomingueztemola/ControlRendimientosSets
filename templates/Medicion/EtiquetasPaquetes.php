1b92fd1246d5f1233549cdf2ddf165294662d1ae
Esteban512 <120593159+Esteban512@users.noreply.github.com>
2023-05-19 09:26:50 -0600

    se subió el botón

--- templates/Medicion/EtiquetasPaquetes.php
+++ templates/Medicion/EtiquetasPaquetes.php
@@ -3,38 +3,41 @@
         <h3 class="text-white">Paquetes</h3>
     </div>
     <div class="card-body">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Seleccion</th>
-                    <th scope="col">Área</th>
-                    <th scope="col">
-                        <button class="btn btn-primary" data-toggle="collapse" data-target="#collapselados"
-                            role="button" aria-expanded="false" aria-controls="collapselados">
-                            Lados
-                        </button>
-                        <div class="collapse" id="collapselados">
-                            <div class="card" style="width: 18rem;">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">12:00 = 123</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>1S</td>
-                    <td>340</td>
-                    <td>
-                        Miedo a la IA
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
+        <button class="btn btn-primary" data-toggle="collapse" data-target="#collapselados" role="button"
+            aria-expanded="false" aria-controls="collapselados">
+            Lados
+        </button>
+        <div class="collapse" id="collapselados">
+            <div class="card" >
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Seleccion</th>
+                                    <th scope="col">Área</th>
+                                    <th scope="col">
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>1S</td>
+                                    <td>340</td>
+                                    <td>
+                                        Miedo a la IA
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
         <div>
             <button class="btn btn-danger btn-lg offset-md-9">Finalizar</button>
         </div>
