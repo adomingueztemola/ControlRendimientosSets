34484e6272cb4f0b50b857e984d2da3a7d6f661a
adomingueztemola <103951211+adomingueztemola@users.noreply.github.com>
2023-05-29 14:31:29 -0600

    Update CrearPaquete.php

--- templates/Medicion/CrearPaquete.php
+++ templates/Medicion/CrearPaquete.php
@@ -105,7 +105,7 @@ $id = !empty($_POST['id']) ? $_POST['id'] : "";
             }
             $("#tbody-lados").html(tabla);
             conteoChecks()
-            alert( $('#tbody-lados').find('.accordionPaq').html())
+            console.log( $('#tbody-lados').find('.accordionPaq').html())
             $('#tbody-lados').find('.accordionPaq').remove()
 
         },
